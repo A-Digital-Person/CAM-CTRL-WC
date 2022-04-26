ada4c2091a3010ee675254bcdf5c7345f4d89069
A-Digital-Person <77813120+A-Digital-Person@users.noreply.github.com>
2022-04-26 10:53:27 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#Thinking of what libs I might need
+import cv2 #Image data
+import yaml #Data mangement
+import time #Delay/Logging
+import numpy #Image data
+import os #File mangement
+
+def capcolor():
+    pass
+
+def cutscreen():
+    pass
+
+def finddriection():
+    pass
+
+def logwarn(msg):
+    pass
+
+def logerror(msg):
+    pass
+
+def loginfo(msg):
