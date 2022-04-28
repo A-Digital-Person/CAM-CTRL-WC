8e453aeaf3f597b363108649b0ae38db414c9836
A-Digital-Person <77813120+A-Digital-Person@users.noreply.github.com>
2022-04-28 11:35:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,3 +38,56 @@ def testbuttin(din, state):
         else:
             output =  False
     return output
+
+def path(use):
+    if use == 'arcleft':
+        leftM.setSpeed(maxspeed/1.5)
+        rightM.setSpeed(maxspeed)
+        
+        leftM.run(hat.FORWARD)
+        rightM.run(hat.FORWARD)
+    elif use == 'arcright':
+        leftM.setSpeed(maxspeed)
+        rightM.setSpeed(maxspeed/1.5)
+        
+        leftM.run(hat.FORWARD)
+        rightM.run(hat.FORWARD)
+    elif use == 'rarcleft':
+        leftM.setSpeed(maxspeed/1.5)
+        rightM.setSpeed(maxspeed)
+        
+        leftM.run(hat.BACKWARD)
+        rightM.run(hat.BACKWARD)
+    elif use == 'rarcright':
+        leftM.setSpeed(maxspeed)
+        rightM.setSpeed(maxspeed/1.5)
+        
+        leftM.run(hat.BACKWARD)
+        rightM.run(hat.BACKWARD)
+    elif use == '0left':
+        leftM.setSpeed(maxspeed/2)
+        rightM.setSpeed(maxspeed/2)
+        
+        leftM.run(hat.BACKWARD)
+        rightM.run(hat.FORWARD)
+    elif use == '0right':
+        leftM.setSpeed(maxspeed/2)
+        rightM.setSpeed(maxspeed/2)
+        
+        leftM.run(hat.FORWARD)
+        rightM.run(hat.BACKWARD)
+    elif use == 'forward':
+        leftM.setSpeed(maxspeed)
+        rightM.setSpeed(maxspeed)
+        
+        leftM.run(hat.FORWARD)
+        rightM.run(hat.FORWARD)
+    elif use == 'backward':
+        leftM.setSpeed(maxspeed/1.5)
+        rightM.setSpeed(maxspeed/1.5)
+        
+        leftM.run(hat.BACKWARD)
+        rightM.run(hat.BACKWARD)
+
+def tail:
+    pass
