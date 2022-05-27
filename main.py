b4c62d83a9bc527053ca5a590914dc0d15769999
A-Digital-Person <77813120+A-Digital-Person@users.noreply.github.com>
2022-05-27 11:35:08 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ tailpointsx = [0,0,0,0,0,0,0,0,0,0]
 tailpointsy = [0,0,0,0,0,0,0,0,0,0]
 
 #Sets the max speed the motors will be allowed to move
-maxspeed = 50 #Range of 0 - 255
+maxspeed = 100 #Range of 0 - 255
+#Speed set by Allison
 
 #Sets the motors pins
 hat = Adafruit_MotorHAT(addr=0x60)
@@ -58,7 +59,11 @@ def finddriection(x, way):
             return "rarcright"
         else:
             return "backward"
-        
+    if way == 'zero':
+        if x < 220:
+            return "0left"
+        elif x > 220:
+            return "0right"
 
 #Next three functions print information to the log file for debugging
 #If something goes worng the file can be sent to devs to trbleshoot
@@ -113,114 +118,95 @@ def testbutton(din, state): #din = pin and state = up or down
 def path(use):
     cv2.putText(frame, use, (3,27), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 5, cv2.LINE_AA)
     cv2.putText(frame, use, (3,27), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-
+    
     try:
         if use == 'arcleft':
             leftM.setSpeed(int(maxspeed/1.5))
             rightM.setSpeed(maxspeed)
             
-            leftM.run(hat.FORWARD)
-            rightM.run(hat.FORWARD)
+            leftM.run(hat.BACKWARD)
+            rightM.run(hat.BACKWARD)
         elif use == 'arcright':
             leftM.setSpeed(maxspeed)
             rightM.setSpeed(int(maxspeed/1.5))
             
-            leftM.run(hat.FORWARD)
-            rightM.run(hat.FORWARD)
+            leftM.run(hat.BACKWARD)
+            rightM.run(hat.BACKWARD)
         elif use == 'rarcleft':
             leftM.setSpeed(int(maxspeed/1.5))
             rightM.setSpeed(maxspeed)
             
-            leftM.run(hat.BACKWARD)
-            rightM.run(hat.BACKWARD)
+            leftM.run(hat.FORWARD)
+            rightM.run(hat.FORWARD)
         elif use == 'rarcright':
             leftM.setSpeed(maxspeed)
             rightM.setSpeed(int(maxspeed/1.5))
             
-            leftM.run(hat.BACKWARD)
-            rightM.run(hat.BACKWARD)
-        elif use == '0left':
-            leftM.setSpeed(int(maxspeed/2))
-            rightM.setSpeed(int(maxspeed/2))
-            
-            leftM.run(hat.BACKWARD)
+            leftM.run(hat.FORWARD)
             rightM.run(hat.FORWARD)
-        elif use == '0right':
-            leftM.setSpeed(int(maxspeed/2))
-            rightM.setSpeed(int(maxspeed/2))
+        elif use == '0left':
+            leftM.setSpeed(int(maxspeed/1.3))
+            rightM.setSpeed(int(maxspeed/1.3))
             
             leftM.run(hat.FORWARD)
             rightM.run(hat.BACKWARD)
+        elif use == '0right':
+            leftM.setSpeed(int(maxspeed/1.3))
+            rightM.setSpeed(int(maxspeed/1.3))
+            
+            leftM.run(hat.BACKWARD)
+            rightM.run(hat.FORWARD)
         elif use == 'forward':
             leftM.setSpeed(maxspeed)
             rightM.setSpeed(maxspeed)
             
-            leftM.run(hat.FORWARD)
-            rightM.run(hat.FORWARD)
+            leftM.run(hat.BACKWARD)
+            rightM.run(hat.BACKWARD)
         elif use == 'backward':
             leftM.setSpeed(int(maxspeed/1.5))
             rightM.setSpeed(int(maxspeed/1.5))
             
-            leftM.run(hat.BACKWARD)
-            rightM.run(hat.BACKWARD)
+            leftM.run(hat.FORWARD)
+            rightM.run(hat.FORWARD)
     except Exception as e:
         logerror(str(e) + ' Direction: "' + str(use) + '"')
         
 #Draws a cursor tail to show direction of travel
-def tail():
+def tail(remove):
     global tailpointsx
     global tailpointsy
     global frame
-    
-#     cv2.circle(frame, (int(tailpointsx[9]),int(tailpointsy[9])),12,(0,0,0),-1) #Starts the 'tail' part
-#     cv2.circle(frame, (int(tailpointsx[9]),int(tailpointsy[9])),10,(20,0,0),-1)
-# 
-#     cv2.circle(frame, (int(tailpointsx[8]),int(tailpointsy[8])),10,(0,0,0),-1)
-#     cv2.circle(frame, (int(tailpointsx[8]),int(tailpointsy[8])),8,(40,0,0),-1)
-#     
-#     cv2.circle(frame, (int(tailpointsx[7]),int(tailpointsy[7])),10,(0,0,0),-1)
-#     cv2.circle(frame, (int(tailpointsx[7]),int(tailpointsy[7])),8,(60,0,0),-1)
-# 
-#     cv2.circle(frame, (int(tailpointsx[6]),int(tailpointsy[6])),10,(0,0,0),-1)
-#     cv2.circle(frame, (int(tailpointsx[6]),int(tailpointsy[6])),8,(80,0,0),-1)
-    
-    cv2.circle(frame, (int(tailpointsx[5]),int(tailpointsy[5])),10,(0,0,0),-1)
-    cv2.circle(frame, (int(tailpointsx[5]),int(tailpointsy[5])),8,(0,42,0),-1)
-    
-    cv2.circle(frame, (int(tailpointsx[4]),int(tailpointsy[4])),10,(0,0,0),-1)
-    cv2.circle(frame, (int(tailpointsx[4]),int(tailpointsy[4])),8,(0,85,0),-1)
-    
-    cv2.circle(frame, (int(tailpointsx[3]),int(tailpointsy[3])),10,(0,0,0),-1)
-    cv2.circle(frame, (int(tailpointsx[3]),int(tailpointsy[3])),8,(0,127,0),-1)
-    
-    cv2.circle(frame, (int(tailpointsx[2]),int(tailpointsy[2])),10,(0,0,0),-1)
-    cv2.circle(frame, (int(tailpointsx[2]),int(tailpointsy[2])),8,(0,170,0),-1)
-    
-    cv2.circle(frame, (int(tailpointsx[1]),int(tailpointsy[1])),10,(0,0,0),-1)
-    cv2.circle(frame, (int(tailpointsx[1]),int(tailpointsy[1])),8,(0,212,0),-1)
+
+    if (remove):
+        cv2.circle(frame, (int(tailpointsx[5]),int(tailpointsy[5])),10,(0,0,0),-1)
+        cv2.circle(frame, (int(tailpointsx[5]),int(tailpointsy[5])),8,(0,42,0),-1)
+        
+        cv2.circle(frame, (int(tailpointsx[4]),int(tailpointsy[4])),10,(0,0,0),-1)
+        cv2.circle(frame, (int(tailpointsx[4]),int(tailpointsy[4])),8,(0,85,0),-1)
+        
+        cv2.circle(frame, (int(tailpointsx[3]),int(tailpointsy[3])),10,(0,0,0),-1)
+        cv2.circle(frame, (int(tailpointsx[3]),int(tailpointsy[3])),8,(0,127,0),-1)
+        
+        cv2.circle(frame, (int(tailpointsx[2]),int(tailpointsy[2])),10,(0,0,0),-1)
+        cv2.circle(frame, (int(tailpointsx[2]),int(tailpointsy[2])),8,(0,170,0),-1)
+        
+        cv2.circle(frame, (int(tailpointsx[1]),int(tailpointsy[1])),10,(0,0,0),-1)
+        cv2.circle(frame, (int(tailpointsx[1]),int(tailpointsy[1])),8,(0,212,0),-1)
     
     cv2.circle(frame, (int(tailpointsx[0]),int(tailpointsy[0])),10,(0,0,0),-1) #Draws a dot onto the screen first a black and then a colored circle is drawn on top
     cv2.circle(frame, (int(tailpointsx[0]),int(tailpointsy[0])),8,(255,0,0),-1)
     
-    tailpointsx = tailpointsx[:-1]
-    tailpointsy = tailpointsy[:-1] #Removes last tail point
+    if (remove):
+        tailpointsx = tailpointsx[:-1]
+        tailpointsy = tailpointsy[:-1] #Removes last tail point
 
 #Get the screen size
 #screen = ctypes.windll.user32
 #Reference = screen.GetSystemMetrics(0) | 0 = width 1 = height
 
-#Find a aviable camera
-# cap = cv2.VideoCapture(0)
-# if cap is None or not cap.isOpened():
-#     cap = cv2.VideoCapture(1)
-#     if cap is None or not cap.isOpened():
-#         cap = cv2.VideoCapture(2)
-#         if cap is None or not cap.isOpened():
-#             sys.exit
-            
-cv2.namedWindow("Capture")
-cv2.namedWindow("Mask")
 cv2.namedWindow("Trackbars")
+cv2.namedWindow("Capture")
+#cv2.namedWindow("Mask")
 
 cv2.createTrackbar("H Min", "Trackbars", int(93), 180, lambda x:None)
 cv2.createTrackbar("S Min", "Trackbars", int(54), 255, lambda x:None)
@@ -240,6 +226,8 @@ kernel = numpy.ones((3,3), numpy.uint8)
 
 cap = cv2.VideoCapture(0)
 #Main loop
+
+allow = True
 while True:
     ret, frame = cap.read()
     frame = frame[::3, ::3]
@@ -273,29 +261,34 @@ while True:
     else:
         ctx = 320 #If no color found place on the center of the screen so bot will stop
         cty = 240
-
-        
-    try:
-        tailpointsx.insert(0, int(ctx)) #adds point to tail
-        tailpointsy.insert(0, int(cty))
-    except:
-        logerror("Tail failed to insert " + str(int(ctx)) + " | " + str(int(cty)))
+    if allow:
+        try:
+            tailpointsx.insert(0, int(ctx)) #adds point to tail
+            tailpointsy.insert(0, int(cty))
+        except:
+            logerror("Tail failed to insert " + str(int(ctx)) + " | " + str(int(cty)))
     
-    tail()
-    print(testbutton(25, 'down'))
-    print(testbutton(24, 'down'))
-    if testbutton(25, 'down'):
-        path(finddriection(ctx, 'forward'))
+    tail(allow)
+
+    if testbutton(4, 'down') and testbutton(25, 'down'):
+        path(finddriection(tailpointsx[0], 'zero'))
+        allow = False
     else:
-        if testbutton(24, 'down'):
-            path(finddriection(ctx, 'backward'))
+        if testbutton(4, 'down'):
+            path(finddriection(tailpointsx[0], 'forward'))
+            allow = False
         else:
-            estop()
+            if testbutton(25, 'down'):
+                path(finddriection(tailpointsx[0], 'backward'))
+                allow = False
+            else:
+                estop()
+                allow = True
     
     #frame = cv2.addWeighted(frame, 1, cv2.imread('Arrows.png'),0.4,0)
  
     cv2.imshow("Capture", frame)
-    cv2.imshow("Mask", mask)
+    #cv2.imshow("Mask", mask)
     
     cv2.setWindowProperty("Capture", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     
